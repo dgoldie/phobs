becb23085bee3cf94993f390c1c7eae621cd3639
Joseph Kain <joekain@gmail.com>
2015-10-11 14:30:23 -0700

    Fix builtin test by referencing Phobs' title

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule Phobs.PageControllerTest do
 
   test "GET /" do
     conn = get conn(), "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Phoenix Observer"
   end
 end
