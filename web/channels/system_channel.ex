dddd07eb1ec3658e6cf04d5705de74622264bf04
Joseph Kain <joekain@gmail.com>
2015-10-13 14:59:19 -0700

    Cancel timers when there are no clients connected to the channel

--- web/channels/system_channel.ex
+++ web/channels/system_channel.ex
@@ -3,13 +3,11 @@ defmodule Phobs.SystemChannel do
   require Logger
 
   alias Phobs.SystemChannel.System
+  alias Phoenix.Socket
 
-  def join("phobs:system", message, socket) do
+  def join("phobs:system", _message, socket) do
     Process.flag(:trap_exit, true)
-
-    # Need to do this only once, not per join
-    # Also need to stop the timer when all connections terminate
-    :timer.send_interval(1000, :update)
+    socket = start_timer(socket)
 
     {:ok, socket}
   end
@@ -19,8 +17,30 @@ defmodule Phobs.SystemChannel do
     {:noreply, socket}
   end
 
-  def terminate(reason, _socket) do
+  def terminate(reason, socket) do
     Logger.debug"> leave #{inspect reason}"
+    socket = cancel_timer(socket)
     :ok
+  end
+
+  defp start_timer(socket) do
+    count = socket.assigns[:phobs_system_timer_count]
+    if (count == nil or count == 0) do
+      {:ok, tref} = :timer.send_interval(1000, :update)
+      socket = Socket.assign(socket, :phobs_system_timer_ref, tref)
+      socket = Socket.assign(socket, :phobs_system_timer_count, 1)
+    else
+      socket = Socket.assign(socket, :phobs_system_timer_count, count + 1)
+    end
+    socket
+  end
+
+  defp cancel_timer(socket) do
+    count = socket.assigns[:phobs_system_timer_count]
+    if (count == 1) do
+      tref = socket.assigns[:phobs_system_timer_ref]
+      :timer.cancel(tref)
+    end
+    Socket.assign(socket, :phobs_system_timer_count, count - 1)
   end
 end
